7f4341ea311c461dc514bc188d87641113792833
unknown <nazrulislam66188@gmail.com>
2023-09-13 11:02:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const multer = require("multer");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const path = require("path");
+const { fileURLToPath } = require("url");
+
+require("dotenv").config();
+
+// middlewares
+app.use(express());
+app.use(cors());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+
+// file storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/assets");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+// mongoose setup
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port:${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
